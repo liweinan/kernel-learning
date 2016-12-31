74158f664c479b497a9d025ef7c416d9b7379172
Weinan Li <l.weinan@gmail.com>
2017-01-01 00:38:49 +0800

    .

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 fold -sw 80 $1 > wrapped.md
-pandoc -s --latex-engine=xelatex --template=template.tex wrapped.md -t latex > $1.latex
+#pandoc -s --latex-engine=xelatex --template=template.tex wrapped.md -t latex > $1.latex
+# --highlight-style=tango
 pandoc --latex-engine=xelatex --template=template.tex wrapped.md -t latex -o $1.pdf
+convert -verbose -density 200 $1.pdf -quality 100  -alpha remove -background white out.png
+convert -append -trim out-*.png $1.png
+rm out*.png
+rm *.pdf
+rm wrapped.md
